5aecaea9cd13ec97328b5e2e87c04c6e34ada8ce
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-06-10 22:38:16 +0530

    chore(setup):initialize project structure with CMakeLists.txt and empty mai

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <map>
+int main() {
+    std::cout << "C++ Standard Version: " << __cplusplus << std::endl;
+    return 0;
+}
