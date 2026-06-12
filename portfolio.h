5a095f2a43ef736967018c7e24e87825cb4432f3
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-06-12 21:26:35 +0530

    feat(trade):define Trade struct with all fields and csv serialization method

--- portfolio.h
+++ portfolio.h
@@ -6,13 +6,13 @@ class Portfolio{
 private:
     std::vector<Trade> trade;
     std::map<std::string,double> net_qty;
-    std::map<std::string,double> avd_cost;
+    std::map<std::string,double> avg_cost;
     std::map<std::string,double> realized;
 public:
-    void add_trade();
+    void add_trade(const Trade& t);
     void save();
     void load();
-    void print_positions();
+    void print_positions(double current_price);
     void print_history();
     void print_pnl();
 
